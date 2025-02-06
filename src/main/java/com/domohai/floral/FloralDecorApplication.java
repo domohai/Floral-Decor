015cabccc86433e989c5805f01b2a3157d259e71
domohai <domohai12@gmail.com>
2025-02-06 22:54:20 +0700

    first commit

---
+++ src/main/java/com/domohai/floral/FloralDecorApplication.java
@@ -0,0 +1,13 @@
+package com.domohai.floral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloralDecorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloralDecorApplication.class, args);
+	}
+
+}
